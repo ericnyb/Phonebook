4583facad9a727d487d88856c14d37c1b003af32
ericnyb <ericnyb@aol.com>
2017-12-15 09:38:24 -0500

    Add toast in utility

--- app/src/main/java/com/ericbandiero/phonebook/Utils/PhoneUtility.java
+++ app/src/main/java/com/ericbandiero/phonebook/Utils/PhoneUtility.java
@@ -1,8 +1,17 @@
 package com.ericbandiero.phonebook.Utils;
 
+import android.content.Context;
+import android.view.Gravity;
+import android.widget.Toast;
+
 /**
  * Created by Eric Bandiero on 12/15/2017.
  */
 
 public class PhoneUtility {
+	public static void toastShowLong(Context context,String string){
+		Toast toast=Toast.makeText(context,string,Toast.LENGTH_LONG);
+		toast.setGravity(Gravity.CENTER, 0, 0);
+		toast.show();
+	}
 }
