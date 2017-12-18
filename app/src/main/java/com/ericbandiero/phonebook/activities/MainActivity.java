b70dde89b40373d41ad84ced4f7585a4a76f85fb
ericnyb <ericnyb@aol.com>
2017-12-17 21:31:23 -0500

    Squashed commit of the following:
    commit 2c902456cd8a608022d65c5d570c8559f389d88f
    Author: ericnyb <ericnyb@aol.com>
    Date:   Sun Dec 17 21:30:14 2017 -0500
    
        Refactor permissions
    
        Using dialog instead of snackbar - message gets cut off.
    
    commit ef8bee4b1f2ad8ee543dfd004c59de7e423d179a
    Author: ericnyb <ericnyb@aol.com>
    Date:   Sun Dec 17 21:24:01 2017 -0500
    
        Refactor permissions
    
    commit 738eeacb7cf78e3054084d33eaa54099f78a2ba6
    Author: ericnyb <ericnyb@aol.com>
    Date:   Sun Dec 17 20:24:02 2017 -0500
    
        Add RxJava
    
    commit e664661c8748e744a059371ca4437bfaba887ce1
    Author: ericnyb <ericnyb@aol.com>
    Date:   Sun Dec 17 19:47:44 2017 -0500
    
        Add RxJava
    
    commit 998b5a266a7896444b04d64977015b9f686495a6
    Author: ericnyb <ericnyb@aol.com>
    Date:   Sun Dec 17 18:12:49 2017 -0500
    
        Fix code location
    
    commit 29adf088cd9e58dbb51f9237f4a76acfd60c222b
    Author: ericnyb <ericnyb@aol.com>
    Date:   Sun Dec 17 18:07:24 2017 -0500
    
        Add navigation

--- app/src/main/java/com/ericbandiero/phonebook/activities/MainActivity.java
+++ app/src/main/java/com/ericbandiero/phonebook/activities/MainActivity.java
@@ -3,7 +3,9 @@ package com.ericbandiero.phonebook.activities;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
@@ -33,6 +35,9 @@ public class MainActivity extends AppCompatActivity {
 	private Context contextActivity;
 	final private int request_code_add_contact=1;
 
+	private MainActivityFragment mainActivityFragment;
+
+	private final int REQUEST_CONTACTS=1;
 	@Inject
 	SharedPreferences sharedPreferences;
 
@@ -48,6 +53,8 @@ public class MainActivity extends AppCompatActivity {
 		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
 
+		setTitle("");
+
 		FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 		fab.setOnClickListener(new View.OnClickListener() {
 			@Override
@@ -59,6 +66,8 @@ public class MainActivity extends AppCompatActivity {
 
 		//Dagger
 		PhoneBookApp.app().basicComponent().inject(this);
+
+		mainActivityFragment = (MainActivityFragment) getSupportFragmentManager().findFragmentById(R.id.fragment);
 
 	}
 
@@ -76,10 +85,20 @@ public class MainActivity extends AppCompatActivity {
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
 
+		String title=item.getTitle().toString();
+		if (AppConstant.DEBUG) Log.d(this.getClass().getSimpleName()+">","Menu tile:"+title);
 		//noinspection SimplifiableIfStatement
 		if (id == R.id.action_settings) {
 			UtilityPhone.toastShowLong(this,getString(R.string.setting_not_yet_implemented));
 			return true;
+		}
+
+		if (title.equals(getString(R.string.menu_goto_bottom))){
+			mainActivityFragment.GotoBottomOfList();
+		}
+
+		if (title.equals(getString(R.string.menu_goto_top))){
+			mainActivityFragment.gotoTopOfList();
 		}
 
 		return super.onOptionsItemSelected(item);
@@ -91,7 +110,7 @@ public class MainActivity extends AppCompatActivity {
 		if (requestCode == request_code_add_contact) {
 			// Make sure the request was successful
 			if (resultCode == RESULT_OK) {
-				MainActivityFragment mainActivityFragment = (MainActivityFragment) getSupportFragmentManager().findFragmentById(R.id.fragment);
+
 				mainActivityFragment.insertNewContact(contactInfo.getStringExtra(EXTRA_CONTACT_NAME),contactInfo.getStringExtra(EXTRA_CONTACT_PHONE));
 				// The user picked a contact.
 				// The Intent's data Uri identifies which contact was selected.
